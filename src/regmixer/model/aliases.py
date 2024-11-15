572a9643e67f85c1ed175256d3d6fc04feec2611
Tyler Murray <tylerm@allenai.org>
2024-11-15 12:05:32 -0800

    Remove reduntant or unused config properties

--- src/regmixer/model/aliases.py
+++ src/regmixer/model/aliases.py
@@ -25,29 +25,11 @@ class ModelConfig:
     d_model: int
     n_heads: int
     n_layers: int
-    mlp_ratio: int
-    weight_tying: bool
-    alibi: bool
     rope_theta: int
     flash_attention: bool
-    attention_dropout: float
-    attention_layer_norm: bool
-    include_bias: bool
-    layer_norm_type: str
-    layer_norm_with_affine: bool
     layer_norm_eps: float
-    bias_for_layer_norm: bool
-    attention_layer_norm_with_affine: bool
-    activation_type: str
-    residual_dropout: float
-    embedding_dropout: float
     max_sequence_length: int
     embedding_size: int
-    init_device: str
-    init_fn: str
-    init_std: float
-    init_cutoff_factor: int
-    norm_after: bool
     precision: str
     block_type: TransformerBlockType
     save_interval: int = 1000
@@ -69,29 +51,11 @@ class ModelConfig:
             d_model=768,
             n_heads=12,
             n_layers=12,
-            mlp_ratio=8,
-            weight_tying=False,
-            alibi=False,
             rope_theta=500000,
             flash_attention=True,
-            attention_dropout=0.0,
-            attention_layer_norm=True,
-            include_bias=False,
-            layer_norm_type="rms",
-            layer_norm_with_affine=True,
             layer_norm_eps=1e-6,
-            bias_for_layer_norm=False,
-            attention_layer_norm_with_affine=True,
-            activation_type="swiglu",
-            residual_dropout=0.0,
-            embedding_dropout=0.0,
             max_sequence_length=4096,
             embedding_size=100352,
-            init_device="cuda",
-            init_fn="normal",
-            init_std=0.02,
-            init_cutoff_factor=3,
-            norm_after=True,
             precision="amp_bf16",
             block_type=TransformerBlockType.reordered_norm,
         )
@@ -103,29 +67,11 @@ class ModelConfig:
             d_model=768,
             n_heads=12,
             n_layers=12,
-            mlp_ratio=8,
-            weight_tying=False,
-            alibi=False,
             rope_theta=500000,
             flash_attention=True,
-            attention_dropout=0.0,
-            attention_layer_norm=True,
-            include_bias=False,
-            layer_norm_type="rms",
-            layer_norm_with_affine=True,
             layer_norm_eps=1e-6,
-            bias_for_layer_norm=False,
-            attention_layer_norm_with_affine=True,
-            activation_type="swiglu",
-            residual_dropout=0.0,
-            embedding_dropout=0.0,
             max_sequence_length=4096,
             embedding_size=100352,
-            init_device="cuda",
-            init_fn="normal",
-            init_std=0.02,
-            init_cutoff_factor=3,
-            norm_after=True,
             precision="amp_bf16",
             block_type=TransformerBlockType.reordered_norm,
         )
