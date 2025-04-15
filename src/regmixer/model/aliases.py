055216561c1269b1038cf2986eb8e233b6edcf5d
MayeeChen <mayeefchen@gmail.com>
2025-04-15 16:07:55 -0700

    increase device batch size again

--- src/regmixer/model/aliases.py
+++ src/regmixer/model/aliases.py
@@ -55,7 +55,7 @@ class ModelConfig:
             rope_theta=500_000,
             flash_attention=True,
             max_sequence_length=4096,
-            device_batch_size=16
+            device_batch_size=32
         )
 
     @classmethod
