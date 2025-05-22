1eafa16b27b0a32c56ff8ebfed2651fcd7835c1d
MayeeChen <mayeefchen@gmail.com>
2025-05-22 01:35:25 -0700

    formatting the sourceinstance to take in the flattened mixture --> can launch a swarm now

--- src/regmixer/cli.py
+++ src/regmixer/cli.py
@@ -99,7 +99,7 @@ def launch(config: Path, mixture_file: Optional[Path], dry_run: bool, no_cache: 
                 spinner.ok("✔")
     else:
         mixes = mk_mixes(config, use_cache=(no_cache == False))
-
+        breakpoint()
         if click.confirm("Launch experiment with this set of mixtures?", default=False):
             with yaspin(text="Building experiment group...", color="yellow") as spinner:
                 launch_group = LaunchGroup(
