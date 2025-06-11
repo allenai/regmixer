a2c0f01ac796e431ec54307f68514518189954fb
MayeeChen <mayeefchen@gmail.com>
2025-06-11 16:49:55 -0700

    allow for more advanced specification of fixed source weights

--- src/regmixer/synthesize_mixture.py
+++ src/regmixer/synthesize_mixture.py
@@ -572,7 +572,7 @@ def calculate_priors(
         try:
             with open(cache_path, "r") as f:
                 logger.info(
-                    "Source distribution cache found, using cached values! This can be disabled by setting use_cache=False."
+                    f"Source distribution cache found, using cached values at {cache_path}! This can be disabled by setting use_cache=False."
                 )
                 obj = json.load(f)
                 return (obj["relative_sizes"], obj["total_tokens"], obj["token_counts"])
